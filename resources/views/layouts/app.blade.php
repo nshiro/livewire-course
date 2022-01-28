4561c68e6eb1d6e96e52683c0e6c080a59bf4871
nshiro <14008307+nshiro@users.noreply.github.com>
2022-01-28 16:12:14 +0000

    フルページコンポーネントの作成

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>タイトル</title>
+@livewireStyles
+
+<style>
+input, textarea, button {
+ border: 1px solid gray;
+ padding: 4px !important;
+}
+input[type=submit], input[type=button] {
+  padding: 4px !important;
+}
+td {
+  border:1px solid gray; padding:3px;
+}
+h1 {
+  font-size: 40px !important;
+}
+.status {
+  font-size: 18px;
+  font-weight: bold;
+  color: blue;
+}
+/* Pagination 用 */
+span[aria-current=page] > span {
+  color: white;
+  background-color: green !important;
+}
+</style>
+</head>
+<body>
+
+{{ $slot }}
+
+@livewireScripts
+
+@stack('js')
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Livewire\PostList;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +21,5 @@ Route::get('/', function () {
 Route::get('counter', function () {
     return view('counting');
 });
+
+Route::get('posts', PostList::class);
