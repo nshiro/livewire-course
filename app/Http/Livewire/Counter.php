71d3b438c708bbd8179e70bd7263a829a5896451
nshiro <14008307+nshiro@users.noreply.github.com>
2022-01-28 06:35:29 +0000

    ライフサイクルのフック

--- app/Http/Livewire/Counter.php
+++ app/Http/Livewire/Counter.php
@@ -16,7 +16,56 @@ class Counter extends Component
         if ($this->counter > 100) {
             $this->counter = 1;
         }
+
+        // dump('increment');
     }
+
+    // public function boot()
+    // {
+    //     dump('boot');
+    // }
+
+    // public function booted()
+    // {
+    //     dump('booted');
+    // }
+
+    // public function mount()
+    // {
+    //     $this->counter = random_int(1, 100);
+
+    //     dump('mount');
+    // }
+
+    // public function hydrate()
+    // {
+    //     dump('hydrate');
+    // }
+
+    // public function deHydrate()
+    // {
+    //     dump('deHydrate');
+    // }
+
+    // public function updating($name, $value)
+    // {
+    //     dump('updating - '. $name.' → '.$value);
+    // }
+
+    // public function updatingName($value)
+    // {
+    //     dump('updatingName - '.$value);
+    // }
+
+    // public function updatingCounter($value)
+    // {
+    //     dump('updatingCounter - '.$value);
+    // }
+
+    // public function updated($name, $value)
+    // {
+    //     dump('updated - '. $name.' → '.$value);
+    // }
 
     public function render()
     {
