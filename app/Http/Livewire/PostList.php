e85176fb1a2af9605838401848dce672004fb236
nshiro <14008307+nshiro@users.noreply.github.com>
2022-02-01 01:34:54 +0000

    リアルタイム検索

--- app/Http/Livewire/PostList.php
+++ app/Http/Livewire/PostList.php
@@ -7,19 +7,29 @@ use Livewire\Component;
 
 class PostList extends Component
 {
-    public $posts;
+    // public $posts;
     public $word;
 
-    public function mount()
-    {
-        $this->posts = Post::get();
-    }
+    // public function mount()
+    // {
+    //     $this->posts = Post::get();
+    // }
+
+    // public function updatedWord($value)
+    // {
+    //     $this->posts = Post::where('title', 'LIKE', '%'.$value.'%')->get();
+    // }
 
     public function render()
     {
+        $posts = Post::query()
+            ->when($this->word, fn($query, $value) => $query->where('title', 'LIKE', '%'.$value.'%'))
+            ->get();
+
         return view('livewire.post-list', [
             // 'posts' => Post::get(),
-            'posts' => $this->posts,
+            // 'posts' => $this->posts,
+            'posts' => $posts,
         ]);
     }
 }
