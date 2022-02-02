ff1bea77481aa0137ad3bb883e2040210b6fcec1
nshiro <14008307+nshiro@users.noreply.github.com>
2022-02-02 06:39:49 +0000

    投稿の登録画面

--- resources/views/livewire/post-list.blade.php
+++ resources/views/livewire/post-list.blade.php
@@ -3,6 +3,24 @@
         検索用語：<input type="text" wire:model="word">
     </form>
 
+    <hr>
+
+    <form wire:submit.prevent="register" class="my-5 bg-sky-100">
+        <div>
+            タイトル：<input type="text" wire:model="title">
+            <div>@error('title')<span style="color:red">{{ $message }}</span>@enderror</div>
+        </div>
+        <div>
+            本文：<textarea wire:model="body" cols="30" rows="5"></textarea>
+            <div>@error('body')<span style="color:red">{{ $message }}</span>@enderror</div>
+        </div>
+        <div>
+            <input type="submit" value="送信する">
+        </div>
+    </form>
+
+    <hr>
+
     <ul>
         @foreach($posts as $post)
         <li wire:key="post-{{ $post->id }}">
