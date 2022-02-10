c087d3ec457efc4f8b99f77ec3a7149477258c84
nshiro <14008307+nshiro@users.noreply.github.com>
2022-02-10 09:26:00 +0000

    SimpleMDE Markdown Editor を導入し、問題点を確認する

--- resources/views/livewire/topic-edit.blade.php
+++ resources/views/livewire/topic-edit.blade.php
@@ -24,11 +24,35 @@
             </div>
             <div>
                 本文：
-                <textarea wire:model.defer="topic.body" cols="30" rows="10"></textarea>
+                <textarea wire:model.defer="topic.body" cols="30" rows="10"
+                    x-data x-init="mdeInit($el)"></textarea>
                 <div>@error('topic.body') <span style="color:red">{{ $message }}</span>@enderror</div>
             </div>
 
             <input type="submit" value="変更する">
         </form>
+
+{{--
+SimpleMDE Markdown Editor
+https://simplemde.com/
+--}}
+
+@push('js')
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/simplemde@1.11.2/dist/simplemde.min.css">
+<script src="https://cdn.jsdelivr.net/npm/simplemde@1.11.2/dist/simplemde.min.js"></script>
+<script>
+function mdeInit(el) {
+    const mde = new SimpleMDE({
+        element: el,
+        spellChecker: false,
+    });
+}
+
+// 問題点
+// 1. 本文がデータ更新されない。
+// 2. タイトルで validation エラーが起こると、エディタが元の姿に戻ってしまう。
+
+</script>
+@endpush
 
     </div>
